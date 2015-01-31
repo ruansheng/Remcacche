9e6403672c023c8de6c75452ed843f79ca95e217
ruansheng <825307513@qq.com>
2015-01-31 21:28:01 +0800

    add hash

--- utils/r_hash.c
+++ utils/r_hash.c
@@ -5,4 +5,59 @@
  *      Author: root
  */
 
+nclude <stdio.h>
+#include "hash.h"
+
+#define INDEX 128
+
+type struct Node Node;
+
+type struct HashTable HashTable;
+
+/**
+* get index
+*/
+int get_index(char *key){
+  if(key==NULL){
+     return -0;
+  }
+  int total=0;
+  while(*a!='\0'){
+    total+=*a;
+    a++;
+  }
+  int index=total%INDEX;
+  return index;
+}
+
+
+/**
+* init HashTable
+*/
+HashTable* h_init(){
+  HashTable *head;
+  head=(HashTable *)malloc(sizeof(HashTable));
+  if(head==NULL){
+    return NULL;
+  }
+  head->next=NULL;
+  return head;
+}
+
+
+void h_insert(HashTable *head，char *key,char *content,long int time_ttl){
+  Node *node;
+  if(head==NULL){
+      return -1;
+  }
+
+}
+
+void h_delete(char *key,HashTable *head){
+
+}
+
+void h_get(char *key,HashTable *head){
+
+}
 
--- utils/r_hash.h
+++ utils/r_hash.h
@@ -4,4 +4,26 @@
  *  Created on: Jan 31, 2015
  *      Author: root
  */
+#include <stdio.h>
 
+/**
+* Node
+*/
+struct Node {
+   char *key;
+   char *content;
+   long int time_ttl;
+   long int size;
+   struct Node *pre;
+   struct Node *next;
+};
+
+/**
+* HashTable
+*/
+struct HashTable {
+   struct Node *node;
+   struct HashTable *pre;
+   struct HashTable *next;
+};
+~               
